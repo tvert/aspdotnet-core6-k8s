368829f5de4ed23a7ccb90cf043f62430c987b25
tvert <tvert@users.noreply.github.com>
2023-05-14 03:18:44 +0000

    Added linux shell script to build docker

--- docker-build-frontend.sh
+++ docker-build-frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Dot not forget -> chmod a+rx docker-build-frontend.sh
+# extra
 docker build -f frontend/Dockerfile --force-rm -t frontend .
 docker tag frontend jorndk8s.azurecr.io/frontend
 echo 'Ready to push -> docker push jorndk8s.azurecr.io/frontend'
