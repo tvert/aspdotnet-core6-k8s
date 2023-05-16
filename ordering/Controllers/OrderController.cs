f80c9c63d1db73993749f66c2e64c228ad2c794a
tvert <tvert@users.noreply.github.com>
2023-05-16 00:02:27 -0700

    - Added Prometheus metrics support to Ordering '\metrics'

--- ordering/Controllers/OrderController.cs
+++ ordering/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using GloboTicket.Ordering.Services;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.AspNetCore.Mvc;
+using Prometheus;
 
 namespace GloboTicket.Ordering.Controllers;
 
@@ -12,30 +13,39 @@ public class OrderController : ControllerBase
 {
     private readonly ILogger<OrderController> logger;
     private readonly EmailSender emailSender;
-    private readonly TelemetryClient telemetryCient;
+    //private readonly TelemetryClient telemetryCient;
+    private static ICounter? _ordersCompleted = null;
 
-    public OrderController(ILogger<OrderController> logger, TelemetryClient telemetryClient, EmailSender emailSender)
+    //public OrderController(ILogger<OrderController> logger, TelemetryClient telemetryClient, EmailSender emailSender)
+    public OrderController(ILogger<OrderController> logger, EmailSender emailSender)
     {
         this.logger = logger;
         this.emailSender = emailSender;
-        this.telemetryCient = telemetryClient;
+        //this.telemetryCient = telemetryClient;
     }
 
     [HttpPost("", Name = "SubmitOrder")]
     public IActionResult Submit(OrderForCreation order)
     {
         logger.LogInformation($"Received a new order from {order.CustomerDetails.Name}");
-        SendAppInsightsTelemetryOrderProcessed();
+        RegisterMetricsOrderProcessed();
+        //SendAppInsightsTelemetryOrderProcessed();
         emailSender.SendEmailForOrder(order);
         return Ok();
     }
 
-    private void SendAppInsightsTelemetryOrderProcessed()
+    private void RegisterMetricsOrderProcessed()
     {
-        MetricTelemetry telemetry = new MetricTelemetry();
-        telemetry.Name = "Order processed";
-
-        telemetry.Count = 1 ;
-        telemetryCient.TrackMetric(telemetry);
+        _ordersCompleted ??= Metrics.CreateCounter("globoticket_orders_processed", "Number of orders completed");
+        _ordersCompleted.Inc();
     }
+
+    //private void SendAppInsightsTelemetryOrderProcessed()
+    //{
+    //    MetricTelemetry telemetry = new MetricTelemetry();
+    //    telemetry.Name = "Order processed";
+
+    //    telemetry.Count = 1 ;
+    //    telemetryCient.TrackMetric(telemetry);
+    //}
 }
